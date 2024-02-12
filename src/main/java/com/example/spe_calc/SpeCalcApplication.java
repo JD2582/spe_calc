cbb1d2ee6222aae138622284dbbc02e7102f106d
JD2582 <darshit.jakhaniya@iiitb.ac.in>
2024-02-12 18:17:56 +0530

    first commit

---
+++ src/main/java/com/example/spe_calc/SpeCalcApplication.java
@@ -0,0 +1,14 @@
+package com.example.spe_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
+@SpringBootApplication
+public class SpeCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeCalcApplication.class, args);
+
+    }
+
+}
